ad36a4cefd62c731392818c06a289ddd9844dadf
lcolco <452440967@qq.com>
2014-03-10 15:48:24 +0800

    Initial Commit

---
+++ LXActionSheetDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LXActionSheetDemo
+//
+//  Created by lixiang on 14-3-10.
+//  Copyright (c) 2014年 iMyQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
